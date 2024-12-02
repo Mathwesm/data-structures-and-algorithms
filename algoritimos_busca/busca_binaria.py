95b4e931d67431b56209a5249a217dadc3909a5c
matheus <matheusssousaribeiro@gmail.com>
2024-12-02 20:53:23 -0300

    add o if __name__ == '__main__' para proteger

--- algoritimos_busca/busca_binaria.py
+++ algoritimos_busca/busca_binaria.py
@@ -16,21 +16,21 @@ def busca_binaria(lista, valor):
 
     return -1
 
+if __name__ == "__main__":
+    lista_ordenada = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+    valor1 = 70
+    resultado, tempo = medir_tempo(busca_binaria, lista_ordenada, valor1, iteracoes=1000)
+    print(f"Elemento {valor1} encontrado no índice: {resultado}, Tempo médio: {tempo:.10f} segundos")
 
-lista_ordenada = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
-valor1 = 70
+    lista_ordenada_grande = list(range(1, 10_001))
+    valor2 = 7777
+    resultado, tempo = medir_tempo(busca_binaria, lista_ordenada_grande, valor2, iteracoes=1000)
+    print(f"Elemento {valor2} encontrado no índice: {resultado}, Tempo médio: {tempo:.10f} segundos")
 
-resultado, tempo = medir_tempo(busca_binaria, lista_ordenada, valor1, iteracoes=1000)
-print(f"Elemento {valor1} encontrado no índice: {resultado}, Tempo médio: {tempo:.10f} segundos")
+    lista_nao_ordenada = lista_ordenada_grande.copy()
+    random.shuffle(lista_nao_ordenada)
+    lista_nao_ordenada.sort()
 
-lista_ordenada_grande = list(range(1, 10_001))
-valor2 = 7777
-resultado, tempo = medir_tempo(busca_binaria, lista_ordenada_grande, valor2, iteracoes=1000)
-print(f"Elemento {valor2} encontrado no índice: {resultado}, Tempo médio: {tempo:.10f} segundos")
-
-lista_nao_ordenada = lista_ordenada_grande.copy()
-random.shuffle(lista_nao_ordenada)
-
-valor3 = 7777
-resultado, tempo = medir_tempo(busca_binaria, lista_nao_ordenada, valor3, iteracoes=1000)
-print(f"Elemento {valor3} encontrado no índice: {resultado}, Tempo médio: {tempo:.10f} segundos")
+    valor3 = 7777
+    resultado, tempo = medir_tempo(busca_binaria, lista_nao_ordenada, valor3, iteracoes=1000)
+    print(f"Elemento {valor3} encontrado no índice: {resultado}, Tempo médio: {tempo:.10f} segundos")
